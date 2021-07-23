ba3aac553b1a85090a24e6525d1c6ceb557f42a7
João Pedro <joaof082@gmail.com>
2021-07-23 00:28:58 -0300

    Creating the component button
    Signed-off-by: João Pedro <joaof082@gmail.com>

---
+++ src/Components/Button/button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export function Button({ children, onClick }) {
+  return (
+    <div>
+      <button
+        onClick={onClick}  
+      >  
+        {children}
+      </button>
+    </div>
+  )
+}
